4bbb630fdfb58104f99acdfb8150d608e5946ee3
EoRichas <Richard.fsoarees@gmail.com>
2026-08-20 15:49:25 -0300

    Exibir prioridade somente nas solicitações

--- rota-proxima/static/service-worker.js
+++ rota-proxima/static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rota-proxima-producao-completa-20260820-v1';
+const CACHE='rota-proxima-producao-completa-20260820-v2';
 const ASSETS=['/','/index.html','/styles.css','/workflow-patch.css','/dashboard-center.css','/ui-cleanup.css','/mobile-access.css','/app.js','/workflow-patch.js','/dashboard-center.js','/ui-cleanup.js','/mobile-access.js','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
