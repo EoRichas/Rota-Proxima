4e9b015d9cef8ff653fa83a5e3c1444b6ed28e92
EoRichas <Richard.fsoarees@gmail.com>
2026-09-17 13:53:49 -0300

    chore: invalidar cache da interface atualizada

--- rota-proxima/static/service-worker.js
+++ rota-proxima/static/service-worker.js
@@ -1,6 +1,6 @@
 /* PWA com atualização segura: APIs sempre passam pela rede; a tela de login
    nunca fica presa em uma cópia antiga do cache. */
-const CACHE_NAME = 'rota-proxima-shell-20260916-11';
+const CACHE_NAME = 'rota-proxima-shell-20260917-2';
 const INDEX_CACHE_KEY = '/index.html';
 
 self.addEventListener('install', event => {
